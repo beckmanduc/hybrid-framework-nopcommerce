7f566b1e791f1e910cc93b80afef8becb4079be7
lê đức <levuanhduc1996@gmail.com>
2021-12-15 01:39:08 +0700

    Config before class to BaseTest and setup auto update driver with lib WebDriverManager

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,37 @@
 package commons;
 
-public class BaseTest {
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public abstract class BaseTest {
+	private WebDriver driver;
+//	private String projectPath = System.getProperty("user.dir");
+
+	protected WebDriver getBrowserDriver(String browserName, String urlValue) {	
+		BrowserList browser = BrowserList.valueOf(browserName.toUpperCase());
+		if (browser == BrowserList.CHROME) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser == BrowserList.FIREFOX) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (browser == BrowserList.EDGE) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else {
+			throw new RuntimeException("This browser is not supported");
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(urlValue);
+		return driver;
+	}
 
 }
